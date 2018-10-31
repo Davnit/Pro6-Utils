d07f23d9645c90869219b2a9b02ffb5e310f2cc9
Davnit <davnit31@gmail.com>
2018-10-31 16:20:36 -0400

    Support multiple files/folders being added
    Make creating documents more flexibile by allowing any number of files or folders to be added instead of just a single one.

--- scripts/create_document.py
+++ scripts/create_document.py
@@ -1,8 +1,8 @@
 
-from pro6 import Pro6Document, MediaCue, get_system_preferences
+from pro6 import Pro6Document, MediaCue
 
-from os import curdir, listdir
-from os.path import basename, splitext, isdir, isabs, join
+from os import listdir
+from os.path import basename, splitext, isdir, isfile, join
 from sys import argv, exit
 
 
@@ -11,43 +11,32 @@ def import_file(document, file_path):
     try:
         document.append(MediaCue(file_path))
         print("success!")
-    except ValueError as ex:
+    except Exception as ex:
         print("failed: %s" % ex)
 
 
 if len(argv) < 2:
-    print("Invalid arguments, format: <document name> [media path]")
+    print("Invalid arguments, format: <document name> [files...]")
     exit(1)
 
-doc_name = splitext(argv[1])[0]
-if not doc_name.endswith(".pro6"):
-    doc_name += ".pro6"
-
-import_path = None if len(argv) < 3 else argv[2]
+doc_name = argv[1]
+files = [] if len(argv) == 2 else argv[2:]
 
 # Create the new document
 print("Creating document: %s" % splitext(basename(doc_name))[0])
-doc = Pro6Document()
-if import_path is not None:
-    # Are we importing a directory or just a single file?
-    if isdir(import_path):
-        for file in listdir(import_path):
-            import_file(doc, join(import_path, file))
-    else:
-        import_file(doc, import_path)
+doc = Pro6Document(doc_name)
 
-print()
-doc.print_outline()
-print()
+# Import the specified files
+if len(files) > 0:
+    for file in files:
+        if isdir(file):
+            for sf in listdir(file):
+                import_file(doc, join(file, sf))
+        elif isfile(file):
+            import_file(doc, file)
+        else:
+            print("The specified file '%s' could not be found." % file)
 
-# Find the library or location to put the document.
-if isabs(doc_name):
-    # Document name specifies absolute path.
-    doc.save(doc_name)
-else:
-    # Use the current library (if available) or current directory.
-    pref = get_system_preferences()
-    lib = pref.general.library_path if pref.exists else curdir
-    doc.save(join(lib, doc_name))
-
-print("Document saved as '%s'." % doc_name)
+# Save the document
+doc.save()
+print("Document saved to '%s'." % doc.path)
