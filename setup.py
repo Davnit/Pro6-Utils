70490ce6d5682178e9f133889849155e70a1397b
Davnit <davnit31@gmail.com>
2018-10-31 23:12:52 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='pro6_utils',
+    version='1.0',
+    description='Utilities for manipulating ProPresenter 6 and its files.',
+    author='Davnit',
+    author_email='david@davnit.net',
+    install_requires=['hachoir']
+)
